0ef1cfba2fc82b7b07982e21fe3e7065e22aafbf
unknown <mintenko.vova@gmail.com>
2020-03-25 09:55:40 +0200

    lab2

--- lab2/User/main.h
+++ lab2/User/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include "stm32f4xx.h"
-#include "stm32f4xx_conf.h"
-#include "misc.h"
-
-#define BUTTON_READ()			GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)
+#include "stm32f4xx_hal.h"
+#include "LCD.h"
 
 #endif
---
+++ lab2/User/pictures.h
@@ -0,0 +1,6 @@
+char message1[] = {'h','e','l','l','o'};
+char message2[] = {'w','o','r','l','d'};
+char message3[] = {'h','e','l','l','o'};
+char message4[] = {'w','o','r','l','d'};
+char message5[] = {'h','e','l','l','o'};
+
